45c29bc462212fd90057b244e3f04cdc6ba89c73
BhaveshArde <bhavesharde02@gmail.com>
2026-08-13 11:31:58 +0530

    JavaScript Practice

---
+++ 12_loops.js
@@ -0,0 +1,20 @@
+// for loop
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+//     if (i==5){
+//         console.log("That is '5'");
+//     }
+// }  // it gives all element one by one 
+
+// for(let i=0;i<5;i++){
+//     for(let j=0;j<5;j++){
+//         console.log(`inner loop ${i} outer loop ${j}`);
+        
+//     }
+// }           // loop within loop "j" run "i" times
+
+// let myArray=["ironman","batman","spiderman","antman","thor","captain"];
+// myArray.forEach((value)=>{
+//     console.log(value);
+// })
+
