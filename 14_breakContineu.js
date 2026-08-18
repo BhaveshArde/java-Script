aa26826c238e813fb2ca6abe6d67dff2717a44bc
BhaveshArde <bhavesharde02@gmail.com>
2026-08-18 11:27:22 +0530

    JavaScript Practice

---
+++ 14_breakContineu.js
@@ -0,0 +1,21 @@
+//------------------  Break  --------------------------------
+for(let i=0;i<10;i++){
+    let no=i;
+    console.log("no:",no);
+    if(no==5){
+        console.log("5 is the best no");
+        
+        break;
+    }
+}
+
+//-----------------  Contineu  ------------------------------
+for(let i=0;i<=5;i++){
+    console.log(i);
+    if(i==3){
+        console.log("that is 3");
+        
+        continue;
+    }
+    
+}
